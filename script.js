c2a8784912d628fc45e25d430273969b9cf5975f
vincaf <vincenzocafassog@gmail.com>
2022-06-14 16:49:14 +0200

    log JS ticket reduction more understandable

--- script.js
+++ script.js
@@ -15,14 +15,15 @@ console.log(userTicket);
 // Condizione per ricevere lo sconto sul biglietto in base all'età
 if (userAge < 18) {
     userTicket = (userTicket - ( (userTicket * 20) / 100 ) );
-    console.log(userTicket);
+    console.log(`Sconto 20% per minorenni ${userTicket}`);
 } else if (userAge > 65) {
     userTicket = (userTicket - ( (userTicket * 40) / 100 ) );
-    console.log(userTicket);
+    console.log(`Sconto 40% per over65 ${userTicket}`);
 }
 
 // Arrotondamento del prezzo biglietto a due cifre decimali
 userTicket = userTicket.toFixed(2);
+console.log(`Prezzo arrotondato ${userTicket}`)
 
 // Scrittura delle variabili sulla pagina
 document.getElementById('userKm').innerHTML = userKm;
