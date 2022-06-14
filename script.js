c428f6b314b4f62650340b278ae72b11e64b687a
vincaf <vincenzocafassog@gmail.com>
2022-06-14 15:58:36 +0200

    HTML inserted and JS variables initialized

---
+++ script.js
@@ -0,0 +1,10 @@
+const userKm = parseInt( prompt('Inserisci i km che devi percorrere') );
+const userAge = parseInt( prompt('Inserisci la tua età') );
+
+console.log(userKm);
+console.log(userAge);
+
+const userTicket = (0.21 * userKm);
+
+console.log(userTicket);
+
